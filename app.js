4cd7371e921761d74bc53e55a94ac89d879d5366
John Ryan <jcryan85@gmail.com>
2014-03-10 15:31:36 -0700

    configured json object to display in html from rotten tomatoes

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
-// Define a new module. This time we declare a dependency on
-// the ngResource module, so we can work with the Instagram API
-
 var app = angular.module("movieApp", ['ngResource']);
 
-// Create and register the new "instagram" service
 app.factory('rotten_tomatoes', function($resource){
 
 	return {
 		fetchMovie: function(callback){
 
-			// The ngResource module gives us the $resource service. It makes working with
-			// AJAX easy. Here I am using the client_id of a test app. Replace it with yours.
-
-			var api = $resource('https://api.instagram.com/v1/media/popular?client_id=:client_id&callback=JSON_CALLBACK',{
+			var api = $resource('http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=:key&q=:movie_title&page_limit=1&callback=JSON_CALLBACK', {
+				key: 'dpjxf3xsjbpj5wpmduveeseb',
 				movie_title: 'Batman'
-			},{
-				// This creates an action which we've chosen to name "fetch". It issues
-				// an JSONP request to the URL of the resource. JSONP requires that the
-				// callback=JSON_CALLBACK part is added to the URL.
-
+			}, {
 				fetch:{method:'JSONP'}
 			});
 
 			api.fetch(function(response){
 
-				// Call the supplied callback function
-				callback(response.data);
+				console.log(response);
+				callback(response);
 
 			});
 		}
 	}
 
+});
+
+app.controller('movieController', function($scope, rotten_tomatoes){
+
+	rotten_tomatoes.fetchMovie(function(data){
+		console.log("Yo bro, movie is Dark Knight");
+		var result = data.movies; 
+		var movie = result[0].title; 
+		console.log(movie);
+		$scope.movie = movie;
+		$scope.result = result;
+	});
 });
--- js/controllers/moviecontroller.js
+++
@@ -1,8 +0,0 @@
-app.controller('movieController.js', function($scope, rotten_tomatoes){
-
-	rotten_tomatoes.fetchMovie(function(data){
-
-
-
-	});
-});
